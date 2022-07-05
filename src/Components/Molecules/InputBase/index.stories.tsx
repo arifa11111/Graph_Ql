b7b2faa53f9f7e2de613a54a8241266479fd2b30
msaisrinivas <100751823+msaisrinivas@users.noreply.github.com>
2022-07-05 10:13:51 +0530

    feat: created InpuBase molecule and Chips atoms (#4)
    * feat: created InpuBase molecule and Chips atoms
    
    * Update: Changed according to comments in InpuBase molecule and Chips atoms
    
    * sonar: Chnaged Buil yaml for sonar
    
    * sonar: Chnaged Buil yaml for sonar 2nd try
    
    * sonar: Chnaged Buil yaml for sonar 3rd try
    
    * sonar: Chnaged Buil yaml for sonar Commit
    
    * test: test for InputBase and Chips

---
+++ src/Components/Molecules/InputBase/index.stories.tsx
@@ -0,0 +1,25 @@
+import CityInput from ".";
+import { ComponentStory } from "@storybook/react";
+import { areas, cities } from "../../../Data/Cities";
+
+export default {
+    title : "Molecules/CityInput",
+    component : CityInput
+}
+
+const Template:ComponentStory<typeof CityInput> = (args) => ( <CityInput {...args} /> );
+
+export const WhereDoYouStay = Template.bind({});
+WhereDoYouStay.args = {
+    locations:areas,
+    placeholder:"Enter your Location",
+}
+
+export const JobLocation = Template.bind({});
+JobLocation.args = {
+    multiple:true,
+    limitTags:1,
+    size:"medium",
+    locations:cities,
+    placeholder:"Enter Your Job Location",
+}
---
+++ src/Components/Molecules/InputBase/index.tsx
@@ -0,0 +1,119 @@
+import {
+  Autocomplete,
+  InputBase,
+  ThemeProvider,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import React from "react";
+import { theme } from "../../../Theme";
+import Chips from "../../Atoms/Chips";
+import "./index.css";
+
+interface InputProps{
+  multiple?:boolean,
+  limitTags?:number,
+  size?:"medium" | "small",
+  placeholder:string,
+  locations?:any,
+  onChange?: (e:React.SyntheticEvent,value:any) => void,
+
+}
+
+function CityInput(props:InputProps) {
+  const classes = useStyles();
+  const textBorder = classes.inputfield + " " + classes.addborderfocused;
+  const noTextBorder = classes.inputfield + " " + classes.addborder;
+  let borderstyle = noTextBorder;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Autocomplete
+        className={classes.inputfield}
+        multiple={props.multiple}
+        limitTags={props.limitTags}
+        size={props.size}
+        id="combo-box-demo"
+        data-testid="Auto-complete"
+        options={props.locations}
+        getOptionLabel={(option) => option.area}
+        onChange={props.onChange}
+        onBlur={(e) => {
+          if((e.target as HTMLInputElement).value === "" && e.currentTarget.firstChild?.firstChild?.nodeName === "INPUT"){
+          borderstyle = noTextBorder;
+        }
+      else{
+        borderstyle = textBorder;
+      }}}
+        popupIcon={""}
+        renderInput={(params) => {
+          const { InputLabelProps, InputProps, ...rest } = params;
+          return (
+            <InputBase
+              {...InputProps}
+              {...rest}
+              data-testid="input-base"
+              classes={{
+                root: borderstyle,
+                focused: classes.addborderfocused,
+              }}
+              placeholder={props.placeholder}
+            ></InputBase>
+          );
+        }}
+
+        renderTags={(tagValue, getTagProps) => {
+          return tagValue.map((option, index) => (
+            <Chips {...getTagProps({ index })} label={option.area} data-testid="chip-check"/>
+          ));
+        }}
+
+      />
+    </ThemeProvider>
+  );
+}
+
+const useStyles = makeStyles({
+  inputfield: {
+    minHeight: "48px",
+    width:"100%",
+    maxWidth:"400px",
+    borderRadius: "8px",
+    "& .css-iqd1hh-MuiButtonBase-root-MuiChip-root": {
+      backgroundColor: "#E8FFFC" ,
+      borderRadius: "0px",
+    },
+    "& .MuiChip-label":{
+      fontFamily: theme.typography.caption2?.fontFamily,
+      fontWeight: theme.typography.caption2?.fontWeight,
+      fontSize: theme.typography.caption2?.fontSize,
+      lineHeight: theme.typography.caption2?.lineHeight,
+    },
+    "& .MuiAutocomplete-input":{
+      fontFamily: theme.typography.caption2?.fontFamily,
+      fontWeight: theme.typography.caption2?.fontWeight,
+      fontSize: theme.typography.caption2?.fontSize,
+      lineHeight: theme.typography.caption2?.lineHeight,
+      color: theme.palette.black?.three
+    }
+  },
+
+  addborder: {
+    border: "2px solid " + theme.palette.gray?.one,
+    "& .MuiInputBase-input": {
+      marginLeft: "1rem"
+    },
+  },
+
+  addborderfocused: {
+    border: "2px solid " + theme.palette.green?.four,
+    boxShadow: "0px 3px 3px "+ theme.palette.gray?.one,
+    "& .MuiInputBase-input": {
+      marginLeft: "1rem",
+    },
+    "& input::placeholder": {
+      color: "white",
+      }
+  },
+});
+
+export default CityInput;

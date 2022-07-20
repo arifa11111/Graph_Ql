cc648b77e20f43464e34c69f320037f6f03a5cb2
Arifa Shaik <shaikarifa.asha@gmail.co>
2022-07-20 12:18:57 +0530

    coverage above 80

--- src/Components/Pages/LandingUI/index.test.tsx
+++ src/Components/Pages/LandingUI/index.test.tsx
@@ -36,23 +36,40 @@ it("Landing Ui testing Active2", () => {
 
 it("Landing Ui testing Active3", () => {
     fun()
-    const ButtonElement = screen.getByTestId("btnNext");
-    fireEvent.click(ButtonElement)
     const InputBaseElement0 = screen.getByRole("combobox");
-    fireEvent.change(InputBaseElement0,{target:{value:"Hyderabad"}})
+    fireEvent.change(InputBaseElement0,{target:{value:"Ameerpet, Hyderabad, Telangana"}})
     const InputBaseElementAfterFire0 = screen.getAllByRole("option")[0];
     fireEvent.click(InputBaseElementAfterFire0)
+    const ButtonElement = screen.getByTestId("btnNext");
+    fireEvent.click(ButtonElement)
+    const InputBaseElement1 = screen.getByRole("combobox");
+    fireEvent.change(InputBaseElement1,{target:{value:"Hyderabad"}})
+    const InputBaseElementAfterFire1 = screen.getAllByRole("option")[0];
+    fireEvent.click(InputBaseElementAfterFire1)
     const AgainButtonElement = screen.getByTestId("btnNext");
     fireEvent.click(AgainButtonElement)
     const InputBaseElement = screen.getByRole("combobox");
     fireEvent.change(InputBaseElement,{target:{value:"UI/Ux Designer"}})
     const InputBaseElementAfterFire = screen.getAllByRole("option")[0];
     fireEvent.click(InputBaseElementAfterFire)
+    const LogoChangeElement = screen.getByText("Jobs found in Hyderabad");
+    expect(LogoChangeElement).toBeInTheDocument()
+    const AgainButtonElement1 = screen.getByTestId("btnNext");
+    fireEvent.click(AgainButtonElement1)
 })
+
 
 it("Landing Ui testing BackButton", () => {
     fun()
-    expect(window.location.pathname).toBe('/')
-    const LogoChangeElement = screen.getByText("Enter Location to know Time Air Quality Index (AQI)");
+    const InputBaseElement = screen.getByRole("combobox");
+    fireEvent.change(InputBaseElement,{target:{value:"Ameerpet, Hyderabad, Telangana"}})
+    const InputBaseElementAfterFire = screen.getAllByRole("option")[0];
+    fireEvent.click(InputBaseElementAfterFire)
+    const ButtonElement = screen.getByTestId("btnNext");
+    fireEvent.click(ButtonElement) 
+    const BackButtonElement = screen.getByTestId("btnBack");
+    fireEvent.click(BackButtonElement);
+    expect(window.location.pathname).toBe('/home/Ameerpet,%20Hyderabad,%20Telangana')
+    const LogoChangeElement = screen.getByText("Real Time Air Quality Index(AQI) in this location");
     expect(LogoChangeElement).toBeInTheDocument()
 })
